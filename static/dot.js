95cd88608d84c3e2895c60f68d6347baa12cee3d
Mantis <tangchunlinit@163.com>
2018-06-26 08:13:40 +0800

    基本的全局js和nginx server配置

---
+++ static/dot.js
@@ -0,0 +1,30 @@
+/**
+ * before include jquery.js
+ * in this project we include jquery.3.3.1.min.js /static/jquery.3.3.1.min.js
+ */
+$(document).ready(function () {
+    // localhost port:8888
+    $.get("http://127.0.0.1:8888/dig", {
+        "time": gettime(),
+        "url": geturl(),
+        "refer": getrefer(),
+        "ua": getuser_agent()
+    })
+});
+
+function gettime() {
+    var nowDate = new Date();
+    return nowDate.toLocaleString();
+}
+
+function geturl() {
+    return window.location.href;
+}
+
+function getrefer() {
+    return document.referrer;
+}
+
+function getuser_agent() {
+    return navigator.userAgent;
+}
